873e2ca324899debf23a2ea0f761e429128d1707
Luan Lima <luan@yata.dev>
2024-01-31 18:54:26 -0300

    :lipstick: Tatil and sonore feedback

--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from 'react'
-import { Alert, Text, View } from 'react-native'
+import { Alert, BackHandler, Text, View } from 'react-native'
 
 import { useNavigation, useRoute } from '@react-navigation/native'
+import { Audio } from 'expo-av'
+import * as Haptics from 'expo-haptics'
 import { Gesture, GestureDetector } from 'react-native-gesture-handler'
 import Animated, {
   Easing,
@@ -58,6 +60,17 @@ export function Quiz() {
   const route = useRoute()
   const { id } = route.params as Params
 
+  async function playSound(isCorrect: boolean) {
+    const file = isCorrect
+      ? require('../../assets/correct.mp3')
+      : require('../../assets/wrong.mp3')
+
+    const { sound } = await Audio.Sound.createAsync(file, { shouldPlay: true })
+
+    await sound.setPositionAsync(0)
+    await sound.playAsync()
+  }
+
   function handleSkipConfirm() {
     Alert.alert('Pular', 'Deseja realmente pular a questão?', [
       { text: 'Sim', onPress: () => handleNextQuestion() },
@@ -94,10 +107,13 @@ export function Quiz() {
     }
 
     if (quiz.questions[currentQuestion].correct === alternativeSelected) {
+      await playSound(true)
+
       setStatusReply(1)
       setPoints((prevState) => prevState + 1)
       handleNextQuestion()
     } else {
+      playSound(false)
       setStatusReply(2)
       shakeAnimation()
     }
@@ -121,7 +137,9 @@ export function Quiz() {
     return true
   }
 
-  function shakeAnimation() {
+  async function shakeAnimation() {
+    await Haptics.notificationAsync(Haptics.NotificationFeedbackType.Error)
+
     shake.value = withSequence(
       withTiming(3, { duration: 400, easing: Easing.bounce }),
       withTiming(0, undefined, (finished) => {
@@ -212,6 +230,15 @@ export function Quiz() {
     const quizSelected = QUIZ.filter((item) => item.id === id)[0]
     setQuiz(quizSelected)
     setIsLoading(false)
+  }, [])
+
+  useEffect(() => {
+    const backHandler = BackHandler.addEventListener(
+      'hardwareBackPress',
+      handleStop,
+    )
+
+    return () => backHandler.remove()
   }, [])
 
   if (isLoading) {
